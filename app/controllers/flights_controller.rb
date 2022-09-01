57df544bbaa3c2f16b93aedefca748dd186d993c
Alyoshka <alyoshkamyshko@gmail.com>
2022-09-01 10:57:01 -0700

    Allow user to select a flight and continue to the new view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,7 +4,12 @@ class FlightsController < ApplicationController
         @airport_options = Airport.all.map {|airport| [airport.airport_code, airport.id] }
         @num_ticket_options = (1..4).each {|ticket_amount| [ticket_amount, ticket_amount] }
         @flight_date_options = Flight.all.map {|flight| [flight.start.strftime("%d/%m/%Y"), flight.start.strftime("%Y%m%d")] }
-        @found_flights = Flight.found_flights(params)
+        @found_flights =  params.key?("flight_date") ? Flight.found_flights(params) : []
+        @num_tickets = params[:num_tickets]
+        @params = params
     end
 
+    def new
+        @params = params
+    end
 end
