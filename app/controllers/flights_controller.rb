30e2fe72c5743be093c0bda238a2aa24a01d9883
Alyoshka <alyoshkamyshko@gmail.com>
2022-08-31 21:19:37 -0700

    Display user's found flights in index view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,5 +4,7 @@ class FlightsController < ApplicationController
         @airport_options = Airport.all.map {|airport| [airport.airport_code, airport.id] }
         @num_ticket_options = (1..4).each {|ticket_amount| [ticket_amount, ticket_amount] }
         @flight_date_options = Flight.all.map {|flight| [flight.start.strftime("%d/%m/%Y"), flight.start.strftime("%Y%m%d")] }
+        @found_flights = Flight.found_flights(params)
     end
+
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,9 @@
 class Flight < ApplicationRecord
     belongs_to :departure_airport, class_name: "Airport"
     belongs_to :arrival_airport, class_name: "Airport"
+
+    def self.found_flights(params)
+        Flight.where("DATE(start) = ? AND departure_airport_id = ? AND arrival_airport_id = ?",
+            Date.parse(params["flight_date"]), params["departure_airport_id"], params["arrival_airport_id"])
+    end
 end
